2caa35134e330ef216b062462e4854bd8f855e08
Gorka <gogalo@hotmail.com>
2019-03-07 19:22:09 +0100

    Mover noscript al body en el ejercicio 1 y nuevo Ejercicio 3

---
+++ javascript/ejercicio3/js/codigo.js
@@ -0,0 +1,7 @@
+// array de meses
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+// mostramos todos los meses por consola
+for (var i=0; i < meses.length; i++) {
+    console.log( meses[i] );
+}
