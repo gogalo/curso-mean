a7a5b8b64f49a462a46cd0909d47b3aa6b4b19ea
Gorka <gogalo@hotmail.com>
2019-03-08 20:29:09 +0100

    ejercicio 10 pendiente

---
+++ javascript/ejercicio10/js/codigo.js
@@ -0,0 +1,8 @@
+var str = "La ruta nos aporto otro paso natural";
+var esPalindromo = checkEsPalindromo(str);
+
+if (esPalindromo) {
+    console.log(str + " " + "si es un palíndromo");
+} else {
+   console.log(str + " " + "NO es un palíndromo");
+}
---
+++ javascript/ejercicio10/js/funciones.js
@@ -0,0 +1,3 @@
+function checkEsPalindromo(str) {
+    // todo ....
+}
