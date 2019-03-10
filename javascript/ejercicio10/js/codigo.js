75e90f177b0642d5a82a51a935b9e3708314abad
gogalo <gogalo@hotmail.com>
2019-03-10 21:29:49 +0100

    Ejercicio 10 terminado

--- javascript/ejercicio10/js/codigo.js
+++ javascript/ejercicio10/js/codigo.js
@@ -2,7 +2,7 @@ var str = "La ruta nos aporto otro paso natural";
 var esPalindromo = checkEsPalindromo(str);
 
 if (esPalindromo) {
-    console.log(str + " " + "si es un palíndromo");
+    console.log(str + " " + "SI es un palíndromo!!!");
 } else {
    console.log(str + " " + "NO es un palíndromo");
 }
--- javascript/ejercicio10/js/funciones.js
+++ javascript/ejercicio10/js/funciones.js
@@ -1,3 +1,15 @@
-function checkEsPalindromo(str) {
-    // todo ....
+function checkEsPalindromo(str) 
+{    
+    // limpiar la frase, solo alfanumericos
+    var inicial = str.toLowerCase().replace(/[\W_]+/g,"");
+    
+    // le damos la vuelta
+    reverse = inicial.split("").reverse().join("");
+    
+    // comparamos
+    if (inicial === reverse) {
+        return true;
+    } 
+
+    return false;
 }
