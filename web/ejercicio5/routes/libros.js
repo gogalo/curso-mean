2c7d4315eeb5dc1b2f50dcaf4062b0aec4c3e3de
Gorka <gogalo@hotmail.com>
2019-03-22 19:31:52 +0100

    Node.js ejercicio 5, ejemplos rutas para generar un RestFul de un recurso determinado.

---
+++ web/ejercicio5/routes/libros.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var libros = require('../fixtures/libros');
+
+
+/* GET Listado de libros */
+router.get('/', function(req, res, next) {
+  res.json(libros);
+});
+
+
+/* POST nuevo libro */
+router.post('/', function(req, res, next) {  
+  var libro = req.body;
+  res.json(libro);
+});
+
+
+/* GET Obtener un libro por isbn */
+router.get('/:isbn', function(req, res, next) {
+  var isbn = req.params.isbn;
+  var libro = libros.find(libro => libro.isbn === isbn);
+  
+  if (!libro) {
+    res.status(404).send('Not found');
+  }
+
+  res.json(libro);
+});
+
+/* PUT Modificar un libro */
+router.put('/:isbn', function(req, res, next) {
+  var isbn = req.params.isbn;
+  var libro = libros.find(libro => libro.isbn === isbn);
+  
+  if (!libro) {
+    res.status(404).send('Not found');
+  }
+  
+  // datos para modificar 
+  libroModificado = req.body;
+  
+  // modificar el libro
+  libro.name = libroModificado.name;
+  libro.author = libroModificado.author;
+
+  res.json(libro);
+
+});
+
+module.exports = router;
